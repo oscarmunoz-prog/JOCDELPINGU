cc6c3b6efdfcf5d74176cd48497876466fb23cd4
ian-yepa <ianrubiosanchez17@alumnes.ilerna.com>
2026-03-03 16:01:00 +0100

    Controlador Turnos #3

--- ControladorTurnos.java
+++ ControladorTurnos.java
@@ -14,3 +14,7 @@ public class ControladorTurnos {
      public void setJugadores(List<Jugador> jugadores) {
         this.jugadores = jugadores;
     }
+
+     public Jugador getJugadorActual() {
+        return jugadores.get(turnoActual);
+    }
