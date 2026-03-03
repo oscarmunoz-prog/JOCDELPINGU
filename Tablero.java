cee769cfae5a31fba9dc589c78c24138cfd6cda4
oscarmunoz-prog <oscarmunyozlopez@alumnes.ilerna.com>
2026-03-03 16:53:10 +0100

    Constructor public Tablero CREADO

--- Tablero.java
+++ Tablero.java
@@ -2,7 +2,15 @@ package src.JOCDELPINGU.Modelo;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//Declara la clase Tablero
 public class Tablero {
+    //Declara una lista privada que contendrá objetos de tipo Casilla
     private List<Casilla> casillas;
+    //Declara una constante que define el tamaño del tablero
     private static final int TAMANO = 50;
+
+    //Constructor de la clase
+    public Tablero() {
+        casillas = new ArrayList<>();
+        generarTablero();
+    }
