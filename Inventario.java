12fb57c5ff5c7c6ddc48de8cbed0c397079480ee
oscarmunoz-prog <oscarmunyozlopez@alumnes.ilerna.com>
2026-03-03 17:07:08 +0100

    Creador Inventario CREADO

--- Inventario.java
+++ Inventario.java
@@ -9,3 +9,10 @@ public class Inventario {
     private static final int MAX_PECES = 2;
     private static final int MAX_BOLAS = 6;
     private static final int MAX_DADOS = 3;
+    //Constructor de la clase
+    public Inventario() {
+        items = new HashMap<>();
+        items.put(item.TipoItem.PEZ, 2);      // Empiezan con 2 peces
+        items.put(item.TipoItem.BOLA_NIEVE, 0);
+        items.put(item.TipoItem.DADO, 1);      // Empiezan con 1 dado
+    }
