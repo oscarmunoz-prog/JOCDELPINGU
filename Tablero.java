1b0a594cc22f302a12f40108b79c7449b91c0275
oscarmunoz-prog <oscarmunyozlopez@alumnes.ilerna.com>
2026-03-03 16:54:08 +0100

    Funcion generarTablero CREADA

--- Tablero.java
+++ Tablero.java
@@ -14,3 +14,15 @@ public class Tablero {
         casillas = new ArrayList<>();
         generarTablero();
     }
+
+    private void generarTablero() {
+        // Primera casilla siempre pingüino
+        casillas.add(new Casilla(0, TipoCasilla.PINGUINO));
+        
+        // Generar el resto aleatoriamente
+        TipoCasilla[] tipos = TipoCasilla.values();
+        for(int i = 1; i < TAMANO; i++) {
+            int aleatorio = (int)(Math.random() * tipos.length);
+            casillas.add(new Casilla(i, tipos[aleatorio]));
+        }
+    }
